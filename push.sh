d482bf808fa1d5be5655280ab654d3aa4d375948
Brayan Lemus <brayest3@gmail.com>
2018-05-02 17:53:18 -0600

    update stage 2

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/circleci:latest
-aws cloudformation create-stack --stack-name CircleCiTest --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
+aws cloudformation create-stack --stack-name CircleCiTest2 --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
 #aws ecs update-service --cluster CircleCiTest --service GOapp-Test --force-new-deployment
