739d6fae9be7f482e95db968adaaccbe4287d8a5
Brayan Lemus <brayest3@gmail.com>
2018-05-03 10:19:32 -0600

    update stage 6

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pip install --upgrade pip
+pip install awscli
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/circleci:latest
 #aws cloudformation create-stack --stack-name CircleCiTest --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
