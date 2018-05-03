299c7d52c45d5a833f17e8e1f890a22342f0a85f
Brayan Lemus <brayest3@gmail.com>
2018-05-03 17:56:46 -0600

    update stage 13

--- push.sh
+++ push.sh
@@ -4,5 +4,8 @@ pip install --upgrade pip
 pip install awscli
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/circleci:latest
-aws cloudformation create-stack --stack-name CircleCiTest --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
+#aws cloudformation create-stack --stack-name CircleCiTest --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
 #aws ecs update-service --cluster CircleCiTest --service GOapp-Test2 --force-new-deployment --region us-east-1
+TASK=$(aws ecs list-tasks --cluster CircleCiTest --query 'taskArns[0]' --output text --region us-east-1)
+RUNNING=$(aws ecs describe-tasks --cluster CircleCiTest --tasks $TASK --query 'tasks[0].{LS:lastStatus}' --output text --region us-east-1)
+echo $RUNNING
