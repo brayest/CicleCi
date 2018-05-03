f87f6161c7ce5ad1788b8c27a7e78038f9953f17
Brayan Lemus <brayest3@gmail.com>
2018-05-03 16:02:44 -0600

    update stage 12

--- push.sh
+++ push.sh
@@ -4,7 +4,5 @@ pip install --upgrade pip
 pip install awscli
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/circleci:latest
-aws s3 sync $PWD/aws/  s3://brayan-bucket/aws/ --region us-east-1
-aws cloudformation create-stack --stack-name CircleCiTest1 --template-url https://s3.amazonaws.com/brayan-bucket/aws/main.yml --capabilities CAPABILITY_IAM --region us-east-1
-#aws cloudformation create-stack --stack-name CircleCiTest --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
+aws cloudformation create-stack --stack-name CircleCiTest --template-body file://$PWD/aws-fargate.yml  --capabilities CAPABILITY_IAM --region us-east-1
 #aws ecs update-service --cluster CircleCiTest --service GOapp-Test2 --force-new-deployment --region us-east-1
